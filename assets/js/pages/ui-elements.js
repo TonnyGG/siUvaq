2b0d13bad0ba1fafc18118147bdff91134be488c
UvTonny <pg@urvenue.com>
2015-09-01 23:22:13 -0500

    SiUvaq
    Servicio

---
+++ assets/js/pages/ui-elements.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	/* ---------- Tabs ---------- */
+	$('#myTab a:first').tab('show');
+	$('#myTab a').click(function (e) {
+	  e.preventDefault();
+	  $(this).tab('show');
+	});
+})
